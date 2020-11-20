67d8ed7282808c0516d21a44296da8ac8a645f84
Fjoepotibutr <vchrpng@gmail.com>
2020-11-20 18:02:24 +0700

    feat(User): implement crud for user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,34 @@
-import express from 'express'
+import express from "express"
+import User from "../models/user"
 
+const router = express.Router()
 
-const router = express.Router();
+router.get("/",(req,res) => {
+  User.find()
+  .then(user => {
+      res.json(user)
+  })
+  .catch(err => res.status(400).json({ errors : err.errors }))
+})
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post("/", (req, res) => {
+  const { author, password } = req.body.user
+  const user = new User({ author })
+  user.setPassword(password)
+  user
+    .save()
+    .then(userRecord => {
+      res.json({ user: userRecord.toAuthJSON() })
+    })
+    .catch(err => res.status(400).json({ errors: err.errors }))
+})
 
-export default router;
+router.delete('/del',(req,res) => {
+  console.log(req.body)
+
+  User.remove({username : req.body.username})
+  .then(result => res.json({result}))
+  .catch(err => res.status(400).json({ errors: err.errors }))
+})
+
+export default router
