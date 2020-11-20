b9e1680dec51f2f1949c0164942c046e6d054f88
Fjoepotibutr <vchrpng@gmail.com>
2020-11-20 16:49:08 +0700

    initial project

---
+++ server/app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+import indexRoute from './routes/index';
+import usersRoute from './routes/users';
+
+const app = express()
+
+const mongo_uri = "mongodb+srv://joe:sert1s@cluster0.yqxb8.mongodb.net/blog?retryWrites=true&w=majority";
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongo_uri, { useNewUrlParser: true }).then(
+  () => {
+    console.log("[success] task 2 : connected to the database ");
+  },
+  error => {
+    console.log("[failed] task 2 " + error);
+    process.exit();
+  }
+);
+
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRoute);
+
+app.use('/users', usersRoute);
+
+module.exports = app;
