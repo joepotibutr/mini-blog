9d1e1f0542a35acde4ce7c4989d96a387caf75c8
Fjoepotibutr <vchrpng@gmail.com>
2020-11-22 16:31:58 +0700

    fix(Blog): fix edit blog

--- api/server/routes/blog.js
+++ api/server/routes/blog.js
@@ -18,26 +18,10 @@ router.post('/',(req,res) => {
 })
 
 router.put('/edit',(req,res) => {
-    console.log(req.body)
-    Blog.update({ id : req.body.id },req.body)
+    Blog.update({ _id : req.body.id },req.body.data)
     .then(result => res.json({ result }))
     .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }))
 })
-
-router.put("/update",(req,res) => {
-    const arr = req.body
-    arr.forEach(obj  => {
-        return Blog.update(
-            { ...obj },
-            { multi : true }
-        )
-        .then(() => console.log(obj.id))
-    })
-    res.json({})
-})
-
-
-
 
 
 router.delete('/del',(req,res) => {
