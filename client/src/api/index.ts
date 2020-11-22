85d4ebe57e0bfd40b95cc82fde4495a8c721ae80
Fjoepotibutr <vchrpng@gmail.com>
2020-11-22 22:29:10 +0700

    fix(API): update api endpoints

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -6,19 +6,25 @@ export default {
             const { user } = (await axios.post('/auth',{ credentials : { ...data }})).data
             return user
         },
-        createAuthor: async (data: any) => {
-            await axios.post('/user',{ ...data })
+        create: async (data: any) => {
+            const { result } =  (await axios.post('/user', { user : data })).data
+            console.log(result)
         }
     },
     blog: {
-        createBlog: async (data: any) => {
-            await axios.post('/blog',{ ...data })
+        getBlogs: async () => {
+            const { data } = await axios.get('/blogs')
+            return data
+        },
+        createBlog: async (data: any, author: string) => {
+            const { blog } = (await axios.post('/blogs',{ ...data, author })).data
+            return blog
         },
         editBlog : async (id: string , data: any) => {
-            await axios.put('/blog/edit',{ id, data })
+            await axios.put('/blogs/edit',{ id, data })
         },
         deleteBlog: async (id: string) => {
-            await axios.delete('/blog/del', { data: { id }})
+            await axios.delete('/blogs/del', { data: { id }})
         }
     }
 }
