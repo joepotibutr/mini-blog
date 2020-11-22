c99cf4ff06f8b24e636b11a67541486c8b235fb1
Fjoepotibutr <vchrpng@gmail.com>
2020-11-22 19:18:21 +0700

    feat(ArticleCard): create Article card component

--- client/src/components/ArticleCard/index.tsx
+++ client/src/components/ArticleCard/index.tsx
@@ -1,14 +1,49 @@
 import React from 'react'
 import { CardWrapper } from './styled'
+import userLogo from '../../assets/user.svg'
 
-interface Props {
-    
+export interface Card {
+    _id: string;
+    title: string;
+    content: string;
+    author: string;
+    status: string;
+    category: string
+    updatedAt: string;
 }
 
-export default (props: Props) => {
+interface Props {
+    card: Card
+    actions: boolean
+    onEditBlog: (id: string) => void
+    onDeleteBlog: (id: string) => void
+    onOpenBlogModal: (id: string) => void
+}
+
+export default ({ card, actions, onEditBlog, onDeleteBlog, onOpenBlogModal }: Props) => {
+    const updateTime = new Date(card.updatedAt).toDateString()
     return (
         <CardWrapper>
-            A Card
+            <div>
+            <span className="category"><b>{card.category}</b></span>
+            {actions && 
+            <span>
+                <span onClick={() => onOpenBlogModal(card._id)}>
+                EDIT
+                </span>
+                <span onClick={() => onDeleteBlog(card._id)}>
+                X
+                </span>
+            </span>}
+            </div>
+            <div className="title-body">
+                <p className="title">{card.title}</p>
+            </div>
+            <div className="author-body">
+                <span><img className="thumbnail" src={userLogo} /></span>
+                <label className="author">{card.author}</label>
+            </div>
+            <label className="date">{updateTime}</label>
         </CardWrapper>
     )
 }
--- client/src/components/ArticleCard/styled.ts
+++ client/src/components/ArticleCard/styled.ts
@@ -1,9 +1,58 @@
 import styled from 'styled-components'
 
 
-export const CardWrapper = styled.div`
-width: 100px;
-height: 100px;
+export const CardWrapper = styled.li`
 
+    background: #ffffff;
+    border-radius: 6px;
+    box-shadow: 0px 0px 7px 0px rgba(0,0,0,.1);
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+  
+
+    .category {
+        font-size: 13px;
+        text-transform: uppercase;
+        color: #B9B0D1;
+    }
+
+    .author-body {
+        display: flex;
+        align-items: center;
+            .thumbnail {
+            width: 25px;
+            height: 25px;
+            margin-right: 10px;
+        }
+
+            .author {
+            font-size: 15px;
+            color: #AEABB6;
+        }
+    }
+
+    
+
+    .date {
+        font-size: 15px;
+        color: #AEABB6;
+    }
+
+    .title-body {
+        height: 100%;
+        color: #C5C4CB;
+
+        .title {
+            display: -webkit-box;
+            -webkit-line-clamp: 3;
+            -webkit-box-orient: vertical;  
+            overflow: hidden;
+            font-size: 25px;
+        }
+    }
+   
 
 `
